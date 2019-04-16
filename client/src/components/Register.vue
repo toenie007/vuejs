57083c31f7bbeef073b476b1ef4026c1e8964f01
Frank Toendang <frank@toendang.com>
2019-04-16 16:23:35 +0200

    nog te fixen: register niet zichtbaar

---
+++ client/src/components/Register.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <input
+    type="email"
+    name="email"
+    placeholder="email" />
+    <br>
+    <input
+    type="password"
+    name="password"
+    placeholder="password" />
+    <br>
+    <button>Register</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
